5cb2d77d2defc4951e27e4f018bead41c7d59401
ADARI GIRISH KUMAR <adari.girishkumar@gmail.com>
2019-08-21 20:46:38 +0530

    Add files via upload

---
+++ insertSort.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<time.h>
+void insertSort(int arr[] , int n)
+{
+int i,key,j;
+for(i=1;i<n;i++)
+{
+	key=arr[i];
+	j=i-1;
+	while(j>=0 && arr[j]>key)
+	{
+		arr[j+1]=arr[j];
+		j=j-1;	
+	}
+	arr[j+1]=key;
+}
+}
+
+void printArray(int arr[],int n)
+{
+	int i;
+	for(i=0;i<n;i++)
+	{
+		printf("%d",arr[i]);
+	}
+	printf("\n");
+}
+
+int main()
+{
+clock_t start, end;
+double cpu_time_used;
+start = clock();
+int arr[] = {5,2,4,6,1,3};
+int n = sizeof(arr)/sizeof(arr[0]);
+insertSort(arr,n);
+printArray(arr,n);
+end = clock();
+cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+printf("time taken is %f\n",cpu_time_used);
+return 0;
+
+}
